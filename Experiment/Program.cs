393e8c258b8e25bde0c2175d1659ec11eae33746
Kemran <morpeh2005ka@gmail.com>
2024-10-14 00:48:04 +0300

    Сделал 3.0

---
+++ Experiment/Program.cs
@@ -0,0 +1,44 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        try
+        {
+            try
+            {
+                int x = 5;
+                int i = 0;
+                int y = x / i;
+                Console.WriteLine("x={0}, y= {1}", x, y);
+            }
+            catch (System.DivideByZeroException e)
+            {
+                Console.WriteLine("Попытка деления на ноль", e.ToString());
+            }
+            catch (System.FormatException e)
+            {
+                Console.WriteLine("Введено не целое число! Исключение", e.ToString());
+            }
+            catch
+            {
+                Console.WriteLine("Неизвестная ошибка. Перезапустите программу");
+                throw;
+            }
+            finally
+            {
+                Console.WriteLine("Выполнили блок finally");
+            }
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine(ex.Message);
+            Console.WriteLine(ex.StackTrace);
+            Console.WriteLine(ex.TargetSite);
+            Console.WriteLine(ex.InnerException);
+            Console.WriteLine(ex.Source);
+            Console.WriteLine(ex.Data);
+            Console.WriteLine(ex.HelpLink);
+        }
+
+    }
+}
